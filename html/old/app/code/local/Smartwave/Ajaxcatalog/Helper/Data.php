37fccc8378a2a599734c6d7fc5ec3e45c0a43fb4
pflikweert <pflikweert@gmail.com>
2015-05-01 13:14:08 +0200

    Removing local magento

--- html/old/app/code/local/Smartwave/Ajaxcatalog/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Smartwave_Ajaxcatalog_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
-	 
--- html/old/app/code/local/Smartwave/SharingTool/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Smartwave_SharingTool_Helper_Data extends Mage_Core_Helper_Abstract
-{
-    
-}
